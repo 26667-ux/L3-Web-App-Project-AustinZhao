9bfe38314841f941b9917290e21387bf079c1810
26667-ux <26667@burnside.school.nz>
2026-05-22 14:36:38 +1200

    updating game detail page in flask, using abort to update 404 error page, LEFT JOIN to seach in ID

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, abort
 import sqlite3
 
 app = Flask(__name__)
@@ -19,11 +19,30 @@ def search():
     return render_template('index.html', games=games)
 
 @app.route('/game/<int:game_id>')
-def game_detail(game_id)
+def game_detail(game_id):
     conn = sqlite3.connect('database.db')
     conn.row_factory = sqlite3.Row
-    
-
+    cursor = conn.cursor()
+    cursor.execute( """
+                    SELECT 
+                        game.game_id,
+                        game.title,
+                        game.release_date,
+                        game.price, 
+                        game.average_rating, 
+                        category.name AS category_name, 
+                        publisher.name AS publisher_name
+                    FROM game
+                    LEFT JOIN category ON game.category_id = category.category_id
+                    LEFT JOIN publisher ON game.publisher_id = publisher.publisher_id
+                    WHERE game.game_id = ?
+                    """, (game_id,))
+    game = cursor.fetchall()
+    if game is None:
+        conn.close()
+        abort(404)
+    conn.close()
+    return render_template('game_detail.html', game=game)
 
 @app.errorhandler(404)
 def page_not_found(e):
